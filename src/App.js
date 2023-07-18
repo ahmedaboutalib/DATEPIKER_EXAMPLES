7b4a28f9af2efac9c7afdb1194aa2c919135ebb4
Ahmed_Aboutalib <112434648+ahmedaboutalib@users.noreply.github.com>
2023-07-18 11:47:47 +0100

    ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import '@mobiscroll/react/dist/css/mobiscroll.min.css';
+import { Datepicker, Button, Page, localeFr } from '@mobiscroll/react';
+import "./style.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [openPicker, setOpenPicker] = React.useState(false);
+    const [date, setDate] = React.useState(new Date());
+    
+    const show = () => {
+        setOpenPicker(true);
+    };
+    
+    const onClose = () => {
+        setOpenPicker(false);
+    };
+    
+    const inputProps = {
+        className: 'md-mobile-picker-input',
+        placeholder: 'Please Select...'
+    };
+    
+    const boxInputProps = {
+        className: 'md-mobile-picker-box-label',
+        inputStyle: 'box',
+        placeholder: 'Please Select...'
+    };
+    
+    return (
+        <Page>
+          
+
+            <div className="mbsc-grid">
+                <div className="mbsc-form-group">
+                    <div className="mbsc-row">
+                        <div className="mbsc-col-12">
+                            <div className="mbsc-txt-muted md-mobile-picker-header">Use the picker with any inputs & show on focus/click</div>
+                            <Datepicker controls={['time']} select="range" inputComponent="input" inputProps={inputProps} />
+                        </div>
+                    </div>
+                </div>
+                <div className="mbsc-form-group">
+                    <div className="mbsc-row">
+                        <div className="mbsc-col-12 mbsc-txt-muted md-mobile-picker-header">Disable <code>onClick/onFocus</code> and only show on button</div>
+                        <div className="mbsc-col-8">
+                            <Datepicker controls={['calendar']} select="range" inputComponent="input" inputProps={inputProps} showOnClick={false} showOnFocus={false} isOpen={openPicker} onClose={onClose} defaultValue={date} />
+                        </div>
+                        <div className="mbsc-col-4">
+                            <Button variant="outline" color="primary" className="md-mobile-picker-button" onClick={show}>Show picker</Button>
+                        </div>
+                    </div>
+                </div>
+                <div className="mbsc-form-group">
+                    <div className="mbsc-row">
+                        <div className="mbsc-col-12">
+                            <div className="mbsc-txt-muted md-mobile-picker-header">Use the picker with a Mobiscroll input</div>
+                            <Datepicker  controls={['calendar']} select="range" inputProps={boxInputProps} />
+                        </div>
+                    </div>
+                </div>
+                <div className="mbsc-txt-muted md-mobile-picker-header">Use the picker inline in any page</div>
+            </div>
+            <div className="md-mobile-picker-inline">
+                <Datepicker display="inline" controls={['calendar']} select="range" />
+            </div>
+        </Page>
+    ); 
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {legacy_createStore} from "redux";
+import reducer from './reducer';
+import { Provider } from 'react';
+
+
+const store=legacy_createStore(reducer);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+<App />
+
 );
 
 // If you want to start measuring performance in your app, pass a function
